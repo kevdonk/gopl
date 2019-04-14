9b49b4af160cfca2be7da2e58975eb23854d69ba
Kevin Mayo <kevin.mayo@udacity.com>
2019-04-14 14:27:19 -0600

    section 1.5

---
+++ ch1/ex1-9/fetch19.go
@@ -0,0 +1,30 @@
+// Fetch prints the content found at a URL
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
