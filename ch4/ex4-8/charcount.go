421699c59ff940a3214f5eedf8b13af3c8485429
Kevin Mayo <kevin.mayo@udacity.com>
2019-04-23 00:16:18 -0600

    section 4.3

--- ch4/ex4-8/charcount.go
+++ ch4/ex4-8/charcount.go
@@ -1,2 +1,75 @@
 // Exercise 4.8: Modify charcount to count letters, digits, and so on in their Unicode categories, using functions
 // like unicode.IsLetter.
+
+// Charcount computes counts of Unicode characters.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)       // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int    // count of lengths of UTF-8 encodings
+	invalid := 0                       // count of invalid UTF-8 characters
+	typeCounts := make(map[string]int) // counts of each type of letter, digit, etc.
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+		switch {
+		case unicode.IsControl(r):
+			typeCounts["control"]++
+		case unicode.IsPunct(r):
+			typeCounts["punctuation"]++
+		case unicode.IsNumber(r):
+			typeCounts["number"]++
+		case unicode.IsSymbol(r):
+			typeCounts["symbol"]++
+		case unicode.IsSpace(r):
+			typeCounts["space"]++
+		case unicode.IsUpper(r):
+			typeCounts["uppercase letter"]++
+		case unicode.IsLower(r):
+			typeCounts["lowercase letter"]++
+		case unicode.IsPrint(r):
+			typeCounts["printable"]++
+		case unicode.IsGraphic(r):
+			typeCounts["graphic"]++
+		}
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	fmt.Print("\ntype\tcount\n")
+	for i, t := range typeCounts {
+		fmt.Printf("%s\t%d\n", i, t)
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
--- ch4/ex4-9/wordfreq.go
+++ ch4/ex4-9/wordfreq.go
@@ -1,2 +1,36 @@
 // Exercise 4.9: Write a program wordfreq to report the frequency of each word in an input text file.
-// Callinput.Split(bufio.ScanWords)beforethefirstcalltoScantobreaktheinputinto words instead of lines.
+// Call input.Split(bufio.ScanWords) before the first call to Scan to break the input into words instead of lines.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	files := os.Args[1:]
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "wordfreq: %v\n", err)
+			continue
+		}
+		defer f.Close()
+		countWords(f)
+	}
+}
+
+func countWords(f *os.File) {
+	counts := map[string]int{}
+	input := bufio.NewScanner(f)
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	fmt.Print("word\tcount\n")
+	for s, i := range counts {
+		fmt.Printf("%s\t%d\n", s, i)
+	}
+}
